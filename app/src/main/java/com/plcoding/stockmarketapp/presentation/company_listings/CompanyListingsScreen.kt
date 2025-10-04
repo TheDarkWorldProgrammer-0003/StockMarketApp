e027ce50d0ceaa1f24df7aacd19271be4217fce9
Shrey Vashisth <143152184+TheDarkWorldProgrammer-0003@users.noreply.github.com>
2025-10-04 22:16:11 +0530

    .

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -1,4 +1,36 @@
 package com.plcoding.stockmarketapp.presentation.company_listings
 
-class CompanyListingsScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Composable
+@Destination(start = true)
+fun CompanyListingsScreen(
+    navigaion: DestinationsNavigator,
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+) {
+    val swipeRefreshState = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.isRefreshing
+    )
+    val state = viewModel.state
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingsEvent.onSearchQueryChange(it)
+                )
+            }
+        )
+
+    }
 }
